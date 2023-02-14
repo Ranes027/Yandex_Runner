d959b6b7df4db33b8257941584f37dae13e1dd82
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-14 19:44:50 +0300

    Update CollectibleManager.cs

--- Yandex_RunnerGame/Assets/Scripts/Components/CollectibleManager.cs
+++ Yandex_RunnerGame/Assets/Scripts/Components/CollectibleManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectibleManager : MonoBehaviour
 {
     [SerializeField] private int _numberOfCollectiblesInLevel;
 
+    [SerializeField] private TextMeshProUGUI _valueText;
+
     public void AddOne()
     {
         _numberOfCollectiblesInLevel ++;
+        _valueText.text = _numberOfCollectiblesInLevel.ToString();
     }
 }
