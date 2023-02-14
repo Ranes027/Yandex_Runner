39d6b34d134790fef571e494b8d0b24c033c2778
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-14 18:26:20 +0300

    Add CollectibleComponent script

---
+++ Yandex_RunnerGame/Assets/Scripts/Components/CollectibleComponent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleComponent : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 180;
+
+    void Update()
+    {
+        transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
+    }
+}
