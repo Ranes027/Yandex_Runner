22b4ba0982a0c9771a4ed6b65931f00691bcb79f
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-15 20:49:58 +0300

    Update GateComponent.cs

--- Yandex_RunnerGame/Assets/Scripts/Components/GateComponent.cs
+++ Yandex_RunnerGame/Assets/Scripts/Components/GateComponent.cs
@@ -12,4 +12,21 @@ public class GateComponent : MonoBehaviour
     {
         _gateAppearence.UpdateVisual(_deformationType, _value);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerModifier playerModifier = other.attachedRigidbody.GetComponent<PlayerModifier>();
+
+        if (playerModifier != null)
+        {
+            if (_deformationType == DeformationType.Width)
+            {
+                playerModifier.AddWidth(_value);
+            }
+            else if (_deformationType == DeformationType.Height)
+            {
+                playerModifier.AddHeight(_value);
+            }
+        }
+    }
 }
