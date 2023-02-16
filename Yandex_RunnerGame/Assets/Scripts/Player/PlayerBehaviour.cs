509eb0fdbc3d92d2cea472c947038757a76e5158
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-16 14:42:03 +0300

    Update PlayerBehaviour.cs

--- Yandex_RunnerGame/Assets/Scripts/Player/PlayerBehaviour.cs
+++ Yandex_RunnerGame/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private PlayerMovement _playerMove;
+    [SerializeField] private PreFinishBehaviour _preFinishBehaviour;
+    [Space]
+    [SerializeField] private Animator _animator;
 
     private void Start()
     {
         _playerMove.enabled = false;
+        _preFinishBehaviour.enabled = false;
     }
 
     public void Play()
     {
         _playerMove.enabled = true;
+    }
+
+    public void StartPreFinishBehaviour()
+    {
+        _playerMove.enabled = false;
+        _preFinishBehaviour.enabled = true;
+    }
+
+    public void StartFinishBehaviour()
+    {
+        _preFinishBehaviour.enabled = false;
+        _animator.SetTrigger("Dance");
     }
 }
