389b2cc48e61519ad7c6efd3e2f7247fc21aa21b
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-16 15:15:51 +0300

    Update GameManager.cs

--- Yandex_RunnerGame/Assets/Scripts/GameManager.cs
+++ Yandex_RunnerGame/Assets/Scripts/GameManager.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject _startMenu;
+
+    [SerializeField] private TextMeshProUGUI _levelName;
+
+    private void Start()
+    {
+        _levelName.text = SceneManager.GetActiveScene().name;
+    }
 
     public void StartGame()
     {
