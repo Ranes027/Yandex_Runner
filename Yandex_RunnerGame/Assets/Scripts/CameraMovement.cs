d966edf06fef2276bb5c7776d510fac333556d39
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-16 15:09:09 +0300

    Update CameraMovement.cs

--- Yandex_RunnerGame/Assets/Scripts/CameraMovement.cs
+++ Yandex_RunnerGame/Assets/Scripts/CameraMovement.cs
@@ -6,9 +6,14 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
+    private void Start()
+    {
+        transform.parent = null;
+    }
+
     void LateUpdate()
     {
-        if(_target != null)
+        if (_target != null)
         {
             transform.position = _target.position;
         }
