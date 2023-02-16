1d60d9952adf214260e57c91acf4ff7ec7fce3e4
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-02-16 16:51:44 +0300

    Add Shop script

---
+++ Yandex_RunnerGame/Assets/Scripts/Shop.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private CollectibleManager _collectibleManager;
+    [Header("Prices")]
+    [SerializeField] private int _widthPrice = 5;
+    [SerializeField] private int _heightPrice = 5;
+    [Header("Stats Value")]
+    [SerializeField] private int _widthValue = 20;
+    [SerializeField] private int _heightValue = 20;
+    [Header("Stats Text")]
+    [SerializeField] private TextMeshProUGUI _widthText;
+    [SerializeField] private TextMeshProUGUI _heightText;
+
+    private PlayerModifier _playerModifier;
+
+    private void Start()
+    {
+        _playerModifier = FindObjectOfType<PlayerModifier>();
+
+        UpdatePricesInUI();
+    }
+
+    public void BuyWidth()
+    {
+        if (_collectibleManager.NumberOfCollectibles >= _widthPrice)
+        {
+            _collectibleManager.SpendMoney(_widthPrice);
+            Progress.Instance.Coins = _collectibleManager.NumberOfCollectibles;
+            Progress.Instance.Width += _widthValue;
+
+            _playerModifier.SetWidth(Progress.Instance.Width);
+        }
+    }
+
+    public void BuyHeight()
+    {
+        if (_collectibleManager.NumberOfCollectibles >= _heightPrice)
+        {
+            _collectibleManager.SpendMoney(_heightPrice);
+            Progress.Instance.Coins = _collectibleManager.NumberOfCollectibles;
+            Progress.Instance.Height += _heightValue;
+
+            _playerModifier.SetHeight(Progress.Instance.Height);
+        }
+    }
+
+    private void UpdatePricesInUI()
+    {
+        _widthText.text = _widthPrice.ToString();
+        _heightText.text = _heightPrice.ToString();
+    }
+}
